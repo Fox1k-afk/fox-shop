7ee44802cba7a712e565d1d35bfec5927b616dfa
Fox1k-afk <onoprienko.77@gmail.com>
2022-12-01 21:56:23 +0200

    added parody on favorite list

--- baza/src/pages/Favorites.tsx
+++ baza/src/pages/Favorites.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import s from './pagesStyles/Favorites.module.css';
-import favoritesEmpty from '../assets/svg/favorites-empty.svg';
 import { useNavigate } from 'react-router-dom';
+
+import favoritesEmpty from '../assets/svg/favorites-empty.svg';
+import { useAppSelector } from '../hooks/redux';
+import FavoriteProduct from './FavoriteProduct';
+import s from './pagesStyles/Favorites.module.css';
 
 const About = () => {
 	const navigate = useNavigate();
+	const favorites = useAppSelector((state) => state.favorites);
 
 	return (
 		<div className={s.favorites__wrapper}>
@@ -14,31 +18,42 @@ const About = () => {
 						<div className={s.page_title}>
 							<h1>Favorites</h1>
 						</div>
+						{favorites.favoriteItems.length >= 1 && (
+							<div className={s.favorites__reset}>
+								<button>Clear favorites</button>
+							</div>
+						)}
 					</div>
 
 					<div>
 						<div className={s.favorites__products_container}>
-							<div className={s.favorites__loader}>
-								<div className={s.favorites__empty_img}>
-									<img src={favoritesEmpty} alt='empty' />
-								</div>
+							{favorites.favoriteItems.length === 0 ? (
+								<div className={s.favorites__loader}>
+									<div className={s.favorites__empty_img}>
+										<img src={favoritesEmpty} alt='empty' />
+									</div>
 
-								<div className={s.favorites__no_products}>
-									there are no products in favorites
+									<div className={s.favorites__no_products}>
+										there are no products in favorites
+									</div>
+									<div>
+										<button
+											onClick={() => navigate('/parnyam')}
+											className={s.favorites_button}
+										>
+											GO to katalog
+										</button>
+									</div>
 								</div>
-								<div>
-									<button
-										onClick={() => navigate('/parnyam')}
-										className={s.favorites_button}
-									>
-										GO to katalog
-									</button>
-								</div>
-							</div>
+							) : (
+								<>
+									{favorites.favoriteItems.map((product) => (
+										<FavoriteProduct product={product} key={product.id} />
+									))}
+								</>
+							)}
 						</div>
 					</div>
-
-					<div></div>
 				</div>
 			</div>
 		</div>
--- baza/src/store/slices/FavoritesSlice.ts
+++ baza/src/store/slices/FavoritesSlice.ts
@@ -1,10 +1,13 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 export interface favoritesState {
 	favoriteItems: any[];
 }
 
+const item = localStorage.getItem('favoriteItems');
+
 const initialState: favoritesState = {
-	favoriteItems: [],
+	favoriteItems: item ? JSON.parse(item) : [],
 };
 
 const favoritesSlice = createSlice({
@@ -14,14 +17,24 @@ const favoritesSlice = createSlice({
 		addToFavorites(state, action) {
 			const favProd = { ...action.payload };
 			state.favoriteItems.push(favProd);
+
 			localStorage.setItem('favoriteItems', JSON.stringify(state.favoriteItems));
+
+			toast.success(`${favProd.title} added to favorites`, {
+				position: 'bottom-left',
+			});
 		},
+
 		removeFromFavorites(state, action) {
 			const favDelItem = state.favoriteItems.filter(
 				(favItem) => favItem.id !== action.payload.id
 			);
 			state.favoriteItems = favDelItem;
 			localStorage.setItem('favoriteItems', JSON.stringify(state.favoriteItems));
+
+			toast.warning(`${action.payload.title} removed from favorites`, {
+				position: 'bottom-left',
+			});
 		},
 	},
 });
